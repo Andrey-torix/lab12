e3a7c3232a64132ce45f17b4456d2794bf2901ed
Andrey-torix <visotin_andrey@mail.ru>
2021-01-24 15:34:25 +0300

    update Stack

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,3 +1,25 @@
 // Copyright 2020 Andreytorix
 
 #include <iostream>
+
+template <typename T>
+class Stack
+{
+ public:
+  void push(T&& value)
+  {
+
+  };
+  void push(const T& value)
+  {
+
+  };
+  void pop()
+  {
+
+  };
+  const T& head() const
+  {
+
+  };
+};
