cafb515d4d29da69cdb39544ad32d686e6d3ea42
legik777 <75944488+legik777@users.noreply.github.com>
2021-05-31 23:09:24 +0300

    Add files via upload

---
+++ include/menu.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <boost/program_options.hpp>
+#include <string>
+class EApplication {
+public:
+
+	void menu(int argc, const char** argv);
+	int exec();
+	int ar;
+private:
+	boost::program_options::options_description m_desk{ "Доступные опции" };
+	boost::program_options::variables_map m_vm;
+	
+	std::string input_path;
+	int threshold;
+};
